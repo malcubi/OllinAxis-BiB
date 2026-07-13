91a42722e83814460cb926a003ee3ac9b0ee16df
malcubi <malcubi@nucleares.unam.mx>
2026-07-13 12:46:51 -0600

    minor change

--- src/matter/analysis_matter.f90
+++ src/matter/analysis_matter.f90
@@ -1,5 +1,5 @@
 
-  subroutine analysis_matter
+  subroutine analysis_matter(box,level)
 
 ! ********************************************************
 ! ***   CALCULATION OF ANALYSIS VARIABLES FOR OUTPUT   ***
@@ -18,6 +18,8 @@
 
   logical contains
 
+  integer box,level
+
 
 ! ********************************
 ! ***   COMPLEX SCALAR FIELD   ***
@@ -27,8 +29,20 @@
 
 !    Norm of complex scalar field: sqrt(phiR**2 + phiI**2).
 
-     if (allocated(complex_phi_norm)) then
+     if (associated(grid(box,level)%complex_phi_norm)) then
         complex_phi_norm = dsqrt(complex_phiR**2 + complex_phiI**2)
+     end if
+
+!    The integrated boson charge is:
+!
+!                   /             
+!    complex_NB  =  |  complex_Bdens dV
+!                   /
+!
+!    with dV the physical volumen element.
+
+     if (associated(grid(box,level)%complex_NB)) then
+        !call bosonintegral
      end if
 
   end if
