f8d62515ab818fd76cae7be944d3013cd8c0e4d2
Miguel Alcubierre <malcubi@nucleares.unam.mx>
2026-09-16 12:09:19 -0600

    Auxiliary quantities for Maxwell and Proca fields

--- src/matter/auxiliary_matter.f90
+++ src/matter/auxiliary_matter.f90
@@ -176,10 +176,6 @@
      Dr_maxw_phi = diff1r(+1)
      Dz_maxw_phi = diff1z(+1)
 
-!    Drr_maxw_phi = diff2r(+1)
-!    Dzz_maxw_phi = diff2z(+1)
-!    Drz_maxw_phi = diff2rz(+1,+1)
-
 !    Derivatives of Ai.
 
      diffvar => maxw_A_r
@@ -252,6 +248,53 @@
 
      end if
 
+!    We have defined vector quantities with indices as follows:
+!       maxw_A: index down
+!       maxw_E: index up
+!       maxw_B: index up
+
+!    Define the Maxwell vector potential (index up).
+
+     maxw_Au_r = (1.d0/psi4) * (g_A*maxw_A_r + r*g_C*maxw_A_z)
+     maxw_Au_z = (1.d0/psi4) * (r*g_C*maxw_A_r + g_B*maxw_A_z)
+
+!    Define the Maxwell electric field (index down).
+
+     maxw_Ed_r = psi4 * (A*maxw_E_r + r*C*maxw_E_z)
+     maxw_Ed_z = psi4 * (r*C*maxw_E_r + B*maxw_E_z)
+
+!    Define the Maxwell magnetic field (index down).
+
+     maxw_Bd_r = psi4 * (A*maxw_B_r + r*C*maxw_B_z)
+     maxw_Bd_z = psi4 * (r*C*maxw_B_r + B*maxw_B_z)
+
+     if (angmom) then
+
+!       Extra terms to previous ones:
+
+!       Maxwell vector potential (index up).
+
+        maxw_Au_r = maxw_Au_r + (1.d0/psi4) * (r*g_C1*maxw_A_p)
+        maxw_Au_z = maxw_Au_z + (1.d0/psi4) * (g_C2*maxw_A_p)
+
+!       Maxwell electric field (index down).
+
+        maxw_Ed_r = maxw_Ed_r + psi4 * (r**3*C1*maxw_E_p)
+        maxw_Ed_z = maxw_Ed_z + psi4 * (r**2*C2*maxw_E_p)
+
+!       Maxwell magnetic field (index down).
+
+        maxw_Bd_r = maxw_Bd_r + psi4 * (r**3*C1*maxw_B_p)
+        maxw_Bd_z = maxw_Bd_z + psi4 * (r**2*C2*maxw_B_p)
+
+!       Pure angular momentum:
+
+        maxw_Au_p = (1.d0/psi4) * (r*g_C1*maxw_A_r + g_C2*maxw_A_z + (g_H/r**2)*maxw_A_p)
+        maxw_Ed_p = psi4 * (r**3*C1*maxw_E_r + r**2*C2*maxw_E_z + r**2*H*maxw_E_p)
+        maxw_Bd_p = psi4 * (r**3*C1*maxw_B_r + r**2*C2*maxw_B_z + r**2*H*maxw_B_p)
+
+     end if
+
 !    Covariant derivatives. Although the index p is present in chris_rpp,
 !    chris_zpp, chris_prp, chris_pzp, remember that they are nonzero with
 !    even without angular momentum.
@@ -279,7 +322,7 @@
      if (angmom) then
 
 !       Extra terms to previous ones.
-     
+
         CovDr_maxw_A_r = CovDr_maxw_A_r - chris_prr*maxw_A_p
         CovDz_maxw_A_r = CovDz_maxw_A_r - chris_prz*maxw_A_p   ! WARNING! Here we have chris_prz singular
         CovDr_maxw_A_z = CovDr_maxw_A_z - chris_prz*maxw_A_p   ! WARNING! Here we have chris_prz singular
@@ -299,17 +342,17 @@
         CovDp_maxw_B_p = CovDp_maxw_B_p + chris_ppp*maxw_B_p
 
 !       Pure angular momentum.
-       
+
         CovDp_maxw_A_r = - chris_rrp*maxw_A_r - chris_zrp*maxw_A_z - chris_prp*maxw_A_p   ! WARNING! Here we have chris_prp singular
         CovDp_maxw_A_z = - chris_rzp*maxw_A_r - chris_zzp*maxw_A_z - chris_pzp*maxw_A_p
         CovDr_maxw_A_p = CovDp_maxw_A_r
         CovDz_maxw_A_p = CovDp_maxw_A_z
-        
+
         CovDp_maxw_E_r = + chris_rrp*maxw_E_r + chris_rzp*maxw_E_z + chris_rpp*maxw_E_p
         CovDp_maxw_E_z = + chris_zrp*maxw_E_r + chris_zzp*maxw_E_z + chris_zpp*maxw_E_p
         CovDr_maxw_E_p = + chris_prr*maxw_E_r + chris_prz*maxw_E_z + chris_prp*maxw_E_p   ! WARNING! Here we have chris_prp and chris_prz singular
         CovDz_maxw_E_p = + chris_prz*maxw_E_r + chris_pzz*maxw_E_z + chris_pzp*maxw_E_p
-        
+
         CovDp_maxw_B_r = + chris_rrp*maxw_B_r + chris_rzp*maxw_B_z + chris_rpp*maxw_B_p
         CovDp_maxw_B_z = + chris_zrp*maxw_B_r + chris_zzp*maxw_B_z + chris_zpp*maxw_B_p
         CovDr_maxw_B_p = + chris_prr*maxw_B_r + chris_prz*maxw_B_z + chris_prp*maxw_B_p   ! WARNING! Here we have chris_prp and chris_prz singular
@@ -343,16 +386,16 @@
      Dz_proc_AR_r = diff1z(+1)
 
      diffvar => proc_AR_z
-     Dr_proc_AR_z = diff1r(-1)
-     Dz_proc_AR_z = diff1z(+1)
+     Dr_proc_AR_z = diff1r(+1)
+     Dz_proc_AR_z = diff1z(-1)
 
      diffvar => proc_AI_r
      Dr_proc_AI_r = diff1r(-1)
      Dz_proc_AI_r = diff1z(+1)
 
      diffvar => proc_AI_z
-     Dr_proc_AI_z = diff1r(-1)
-     Dz_proc_AI_z = diff1z(+1)
+     Dr_proc_AI_z = diff1r(+1)
+     Dz_proc_AI_z = diff1z(-1)
 
 !    Derivatives of E.
 
@@ -361,16 +404,16 @@
      Dz_proc_ER_r = diff1z(+1)
 
      diffvar => proc_ER_z
-     Dr_proc_ER_z = diff1r(-1)
-     Dz_proc_ER_z = diff1z(+1)
+     Dr_proc_ER_z = diff1r(+1)
+     Dz_proc_ER_z = diff1z(-1)
 
      diffvar => proc_EI_r
      Dr_proc_EI_r = diff1r(-1)
      Dz_proc_EI_r = diff1z(+1)
 
      diffvar => proc_EI_z
-     Dr_proc_EI_z = diff1r(-1)
-     Dz_proc_EI_z = diff1z(+1)
+     Dr_proc_EI_z = diff1r(+1)
+     Dz_proc_EI_z = diff1z(-1)
 
 !    Derivatives of B.
 
@@ -379,16 +422,16 @@
      Dz_proc_BR_r = diff1z(+1)
 
      diffvar => proc_BR_z
-     Dr_proc_BR_z = diff1r(-1)
-     Dz_proc_BR_z = diff1z(+1)
+     Dr_proc_BR_z = diff1r(+1)
+     Dz_proc_BR_z = diff1z(-1)
 
      diffvar => proc_BI_r
      Dr_proc_BI_r = diff1r(-1)
      Dz_proc_BI_r = diff1z(+1)
 
      diffvar => proc_BI_z
-     Dr_proc_BI_z = diff1r(-1)
-     Dz_proc_BI_z = diff1z(+1)
+     Dr_proc_BI_z = diff1r(+1)
+     Dz_proc_BI_z = diff1z(-1)
 
 !    Advective derivatives.
 
@@ -398,11 +441,73 @@
 
      end if
 
+!    We have defined vector quantities with index as follow
+!       proc_A: index down
+!       proc_E: index up
+!       proc_B: index up
+
+!    Define the Proca vector potential (index up).
+
+     proc_ARu_r = (1/psi4) * (g_A*proc_AR_r + r*g_C*proc_AR_z)
+     proc_AIu_r = (1/psi4) * (g_A*proc_AI_r + r*g_C*proc_AI_z)
+     proc_ARu_z = (1/psi4) * (r*g_C*proc_AR_r + g_B*proc_AR_z)
+     proc_AIu_z = (1/psi4) * (r*g_C*proc_AI_r + g_B*proc_AI_z)
+
+!    Define the Proca electric field (index down).
+
+     proc_ERd_r = psi4 * (A*proc_ER_r + r*C*proc_ER_z)
+     proc_EId_r = psi4 * (A*proc_EI_r + r*C*proc_EI_z)
+     proc_ERd_z = psi4 * (r*C*proc_ER_r + B*proc_ER_z)
+     proc_EId_z = psi4 * (r*C*proc_EI_r + B*proc_EI_z)
+
+!    Define the Proca magnetic field (index down).
+
+     proc_BRd_r = psi4 * (A*proc_BR_r + r*C*proc_BR_z)
+     proc_BId_r = psi4 * (A*proc_BI_r + r*C*proc_BI_z)
+     proc_BRd_z = psi4 * (r*C*proc_BR_r + B*proc_BR_z)
+     proc_BId_z = psi4 * (r*C*proc_BI_r + B*proc_BI_z)
+
+     if (angmom) then
+
+!       Extra terms to previous one:
+
+!       Proca vector potential (index up).
+
+        proc_ARu_r = proc_ARu_r + (1/psi4) * (r*g_C1*proc_AR_p)
+        proc_AIu_r = proc_AIu_r + (1/psi4) * (r*g_C1*proc_AI_p)
+        proc_ARu_z = proc_ARu_z + (1/psi4) * (g_C2*proc_AR_p)
+        proc_AIu_z = proc_AIu_z + (1/psi4) * (g_C2*proc_AI_p)
+
+!       Proca electric field (index down).
+
+        proc_ERd_r = proc_ERd_r + psi4 * (r**3*C1*proc_ER_p)
+        proc_EId_r = proc_EId_r + psi4 * (r**3*C1*proc_EI_p)
+        proc_ERd_z = proc_ERd_z + psi4 * (r**2*C2*proc_ER_p)
+        proc_EId_z = proc_EId_z + psi4 * (r**2*C2*proc_EI_p)
+
+!       Proca magnetic field (index down).
+
+        proc_BRd_r = proc_BRd_r + psi4 * (r**3*C1*proc_BR_p)
+        proc_BId_r = proc_BId_r + psi4 * (r**3*C1*proc_BI_p)
+        proc_BRd_z = proc_BRd_z + psi4 * (r**2*C2*proc_BR_p)
+        proc_BId_z = proc_BId_z + psi4 * (r**2*C2*proc_BI_p)
+
+!       Pure angular momentum:
+
+        proc_ARu_p = (1/psi4) * (r*g_C1*proc_AR_r + g_C2*proc_AR_z + (g_H/r**2)*proc_AR_p)
+        proc_AIu_p = (1/psi4) * (r*g_C1*proc_AI_r + g_C2*proc_AI_z + (g_H/r**2)*proc_AI_p)
+        proc_ERd_p = psi4 * (r**3*C1*proc_ER_r + r**2*C2*proc_ER_z + r**2*H*proc_ER_p)
+        proc_EId_p = psi4 * (r**3*C1*proc_EI_r + r**2*C2*proc_EI_z + r**2*H*proc_EI_p)
+        proc_BRd_p = psi4 * (r**3*C1*proc_BR_r + r**2*C2*proc_BR_z + r**2*H*proc_BR_p)
+        proc_BId_p = psi4 * (r**3*C1*proc_BI_r + r**2*C2*proc_BI_z + r**2*H*proc_BI_p)
+
+     end if
+
 !    Covariant derivatives. Although the index p is present in chris_rpp,
 !    chris_zpp, chris_prp, chris_pzp, remember that they are nonzero with
 !    even without angular momentum.
 
-!    Real part
+!    Real part.
 
      CovDr_proc_AR_r = Dr_proc_AR_r - chris_rrr*proc_AR_r - chris_zrr*proc_AR_z
      CovDz_proc_AR_r = Dz_proc_AR_r - chris_rrz*proc_AR_r - chris_zrz*proc_AR_z
@@ -422,7 +527,7 @@
      CovDz_proc_BR_z = Dz_proc_BR_z + chris_zrz*proc_BR_r + chris_zzz*proc_BR_z
      CovDp_proc_BR_p =              + chris_prp*proc_BR_r + chris_pzp*proc_BR_z  ! WARNING! Here we have chris_prp singular
 
-!    Imaginary part
+!    Imaginary part.
 
      CovDr_proc_AI_r = Dr_proc_AI_r - chris_rrr*proc_AI_r - chris_zrr*proc_AI_z
      CovDz_proc_AI_r = Dz_proc_AI_r - chris_rrz*proc_AI_r - chris_zrz*proc_AI_z
@@ -442,7 +547,7 @@
      CovDz_proc_BI_z = Dz_proc_BI_z + chris_zrz*proc_BI_r + chris_zzz*proc_BI_z
      CovDp_proc_BI_p =              + chris_prp*proc_BI_r + chris_pzp*proc_BI_z  ! WARNING! Here we have chris_prp singular
 
-!    We add terms related to angular momentum
+!    We add terms related to angular momentum.
 
      if (angmom) then
 
@@ -496,12 +601,12 @@
         CovDp_proc_AR_z = - chris_rzp*proc_AR_r - chris_zzp*proc_AR_z - chris_pzp*proc_AR_p
         CovDr_proc_AR_p = CovDp_proc_AR_r
         CovDz_proc_AR_p = CovDp_proc_AR_z
-        
+
         CovDp_proc_ER_r = + chris_rrp*proc_ER_r + chris_rzp*proc_ER_z + chris_rpp*proc_ER_p
         CovDp_proc_ER_z = + chris_zrp*proc_ER_r + chris_zzp*proc_ER_z + chris_zpp*proc_ER_p
         CovDr_proc_ER_p = + chris_prr*proc_ER_r + chris_prz*proc_ER_z + chris_prp*proc_ER_p  ! WARNING! Here we have chris_prp, chris_prp singular
         CovDz_proc_ER_p = + chris_prz*proc_ER_r + chris_pzz*proc_ER_z + chris_pzp*proc_ER_p
-        
+
         CovDp_proc_BR_r = + chris_rrp*proc_BR_r + chris_rzp*proc_BR_z + chris_rpp*proc_BR_p
         CovDp_proc_BR_z = + chris_zrp*proc_BR_r + chris_zzp*proc_BR_z + chris_zpp*proc_BR_p
         CovDr_proc_BR_p = + chris_prr*proc_BR_r + chris_prz*proc_BR_z + chris_prp*proc_BR_p  ! WARNING! Here we have chris_prp, chris_prp singular
@@ -513,19 +618,18 @@
         CovDp_proc_AI_z = - chris_rzp*proc_AI_r - chris_zzp*proc_AI_z - chris_pzp*proc_AI_p
         CovDr_proc_AI_p = CovDp_proc_AI_r
         CovDz_proc_AI_p = CovDp_proc_AI_z
-        
+
         CovDp_proc_EI_r = + chris_rrp*proc_EI_r + chris_rzp*proc_EI_z + chris_rpp*proc_EI_p
         CovDp_proc_EI_z = + chris_zrp*proc_EI_r + chris_zzp*proc_EI_z + chris_zpp*proc_EI_p
         CovDr_proc_EI_p = + chris_prr*proc_EI_r + chris_prz*proc_EI_z + chris_prp*proc_EI_p  ! WARNING! Here we have chris_prp, chris_prp singular
         CovDz_proc_EI_p = + chris_prz*proc_EI_r + chris_pzz*proc_EI_z + chris_pzp*proc_EI_p
-        
+
         CovDp_proc_BI_r = + chris_rrp*proc_BI_r + chris_rzp*proc_BI_z + chris_rpp*proc_BI_p
         CovDp_proc_BI_z = + chris_zrp*proc_BI_r + chris_zzp*proc_BI_z + chris_zpp*proc_BI_p
         CovDr_proc_BI_p = + chris_prr*proc_BI_r + chris_prz*proc_BI_z + chris_prp*proc_BI_p  ! WARNING! Here we have chris_prp, chris_prp singular
         CovDz_proc_BI_p = + chris_prz*proc_BI_r + chris_pzz*proc_BI_z + chris_pzp*proc_BI_p
 
      end if
-
 
   end if
 
